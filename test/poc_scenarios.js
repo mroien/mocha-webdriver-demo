3ab691bcec8feb0ec1c3fe591996d4e0f2db54a6
Jukafah <steve.momcilovic@gmail.com>
2015-10-31 12:57:22 -0500

    demo with pom.

--- test/poc_scenarios.js
+++ test/poc_scenarios.js
@@ -1,9 +1,9 @@
 var assert = require('assert'),
     test = require('selenium-webdriver/testing'),
-    webdriver = require('selenium-webdriver');
+    webdriver = require('selenium-webdriver'),
+    tie3 = require('../pom/tie3');
 
 var driver;
-var homePage = 'http://www.tie3.ajc.com';
 
 test.describe('Ad verification', function() {
     this.timeout(30000);
@@ -19,38 +19,44 @@ test.describe('Ad verification', function() {
     });
 
     test.it('home page loads', function () {
-        driver.get(homePage);
+        driver.get(tie3.homePage());
         driver.getTitle().then(function (title) {
-            assert.equal(title, 'AJC.com: Atlanta News, Sports, Atlanta Weather, Business News | www.tie3.ajc.com');
+            assert.equal(title, tie3.homePageTitle());
         });
     });
 
     test.it("RP02 is '300x100'", function () {
-        var RPO2 = driver.findElement(webdriver.By.id('google_ads_iframe_11347122/www.ajc.com-wired/online/home_9'));
-        var height = RPO2.getAttribute('height').then(function (height) {
-            assert.equal(height, 100);
-        });
-        var width = RPO2.getAttribute('width').then(function (width) {
-            assert.equal(width, 300);
-        });
+        var testPassed = false;
+        var RPO2 = driver.findElement(webdriver.By.id(tie3.RP02())).getSize()
+            .then(function(dimensions) {
+                if (dimensions.width == 300 && dimensions.height == 100) {
+                    testPassed = true;
+                }
+                assert.equal(testPassed, true,
+                    'Expected dimensions did not match actual - Width: '+dimensions.width+' Height: '+dimensions.height);
+            });
     });
 
-    // @todo: update with combinations - update to dimensions then verification
     test.it("RP03 has valid dimensions of - '300x250', '600x160', or '302x250'", function () {
-        var RP03 = driver.findElement(webdriver.By.id('google_ads_iframe_11347122/www.ajc.com-wired/online/home_11'));
-        var height = RP03.getAttribute('height').then(function (height) {
-            assert.equal(height, 250);
-        });
-        var width = RP03.getAttribute('width').then(function (width) {
-            assert.equal(width, 300);
-        });
+        var testPassed = false;
+        var RP03 = driver.findElement(webdriver.By.id(tie3.RP03())).getSize()
+            .then(function(dimensions) {
+                if (dimensions.width == 300 && dimensions.height == 250) {
+                    testPassed = true;
+                } else if (dimensions.width == 600 && dimensions.height == 160) {
+                    testPassed = true;
+                } else if (dimensions.width == 302 && dimensions.height == 250) {
+                    testPassed = true;
+                }
+                assert.equal(testPassed, true,
+                    'Expected dimensions did not match actual - Width: '+dimensions.width+' Height: '+dimensions.height);
+            });
     });
 
     test.it("HP AD has valid dimensions of - '728x90', '950x30', or '970x90'", function () {
         var testPassed = false;
-        var dimensions = driver.findElement(webdriver.By.id('google_ads_iframe_11347122/www.ajc.com-wired/online/home_1'))
+        var dimensions = driver.findElement(webdriver.By.id(tie3.HP_AD()))
             .getSize().then(function(dimensions) {
-
                 if (dimensions.width == 728 && dimensions.height == 90) {
                     testPassed = true;
                 } else if (dimensions.width == 950 && dimensions.height == 30) {
@@ -58,21 +64,20 @@ test.describe('Ad verification', function() {
                 } else if (dimensions.width == 970 && dimensions.height == 90) {
                     testPassed = true;
                 }
-
                 assert.equal(testPassed, true,
                     'Expected dimensions did not match actual! Width: '+dimensions.width+' Height: '+dimensions.height);
             });
     });
 
     test.it('Rotator has 4 slots', function() {
-        var rotator = driver.findElements(webdriver.By.className("cm-media-rotator-nav"))
+        var rotator = driver.findElements(webdriver.By.className(tie3.rotator()))
             .then(function(rotator) {
                 assert.equal(rotator.length, 4);
             });
     });
 
     test.it('Rotator has ad NPR04 in 4th slot', function() {
-        var NPR04 = driver.findElement(webdriver.By.id('cm-ad-block-npr04'))
+        var NPR04 = driver.findElement(webdriver.By.id(tie3.NPR04()))
             .getText().then(function(NPR04) {
                 assert.equal(NPR04.split("\n")[0], 'SPONSORED');
             });
@@ -80,37 +85,22 @@ test.describe('Ad verification', function() {
 
     test.it("SWB has valid dimensions of '300x250'", function() {
         var testPassed = false;
-        var dimensions = driver.findElement(webdriver.By.id('google_ads_iframe_11347122/www.ajc.com-wired/online/home_19'))
+        var dimensions = driver.findElement(webdriver.By.id(tie3.SWB()))
             .getSize().then(function(dimensions) {
-                var height = dimensions.height;
-                var width = dimensions.width;
-
-                if (height == 250 && width == 300) {
+                if (dimensions.width == 300 && dimensions.height == 250) {
                     testPassed = true;
                 }
-                assert.equal(testPassed, true, "SWB was sized width: "+width+" Height: "+height);
+                assert.equal(testPassed, true, "SWB was sized width: "+dimensions.width+" Height: "+dimensions.height);
             });
     });
 
     test.it('SWB is only showing in odd rows', function() {
-        var rows = driver.findElement(webdriver.By.className('jscroll-inner'))
+        var rows = driver.findElement(webdriver.By.className(tie3.rows()))
             .then(function() {
-                var temp = driver.findElements(webdriver.By.className('cm-story-card-ad'))
+                var temp = driver.findElements(webdriver.By.className(tie3.storyCard()))
                     .then(function(temp) {
                         assert.equal(temp.length, 1);
                     });
             });
-    });
-
-    test.it('Rotator rotates in < 60 seconds', function() {
-        function fetchActive() {
-            driver.findElements(webdriver.By.className('.cm-slid-active_0, .cm-slide-active_1, .cm-slide-active_2, .cm-slide-active_3, .cm-slide-active_4'))
-                .then(function(el) {
-                    return el;
-                });
-        }
-        var e1 = fetchActive();
-        var e2 = setTimeout(fetchActive(), 20000);
-        assert.notEqual(e1, e2, 'Rotator did not rotate fast enough!');
     });
 });
